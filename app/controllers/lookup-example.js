e3d23d69b6d0b79b842a5cd10f2122935cae9950
Phil Renaud <phil@riotindustries.com>
2023-07-27 11:49:52 -0400

    Number of datapoints slider

--- app/controllers/lookup-example.js
+++ app/controllers/lookup-example.js
@@ -6,7 +6,9 @@ import { tracked } from '@glimmer/tracking';
 export default class LookupExampleController extends Controller {
   @tracked multiplier = 5;
 
+  @tracked numberOfDataPoints = 20;
+
   get data() {
-    return new Array(20).fill(0).map(() => Math.random() * this.multiplier);
+    return new Array(+this.numberOfDataPoints).fill(0).map(() => Math.random() * this.multiplier);
   }
 }
